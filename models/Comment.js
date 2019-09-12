3cfd6be223eb7019e15a14f3f7a62b56d06b3502
Abhinav <singh.abhinav6996@gmail.com>
2019-09-12 21:34:07 +0530

    model update

--- models/Comment.js
+++ models/Comment.js
@@ -2,9 +2,9 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var commentSchema = new Schema({
-  articleId: {},
-  commentField: {},
-  userId: {}
+  articleId: { type: Schema.Types.ObjectId, ref: "Article", required: true },
+  commentField: { type: String, required: true },
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true }
 });
 
 var Comment = mongoose.model("Comment", commentSchema);
