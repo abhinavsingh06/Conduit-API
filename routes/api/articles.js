c2157e01a68d5715e1dda69e8671e9dea68b06c2
Abhinav <singh.abhinav6996@gmail.com>
2019-09-13 18:14:54 +0530

    articles CRUD update

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -5,3 +5,11 @@ var Tag = require("../../models/Tag");
 var Comment = require("../../models/Comment");
 var authToken = require("../../modules/authToken");
 var router = express.Router();
+
+//display articles
+router.get("/", (req, res, next) => {
+  Article.find({}, (err, allArticles) => {
+    if (err) return res.json({ msg: "Couldn't fetch articles" });
+    return res.json({ allArticles });
+  });
+});
