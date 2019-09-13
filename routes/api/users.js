0814c8d4d0ba37e421160d43950504614995d80a
Abhinav <singh.abhinav6996@gmail.com>
2019-09-13 17:57:40 +0530

    routes update

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,7 @@ var jwt = require("jsonwebtoken");
 var authToken = require("../../modules/authToken");
 var router = express.Router();
 
-/* GET users listing. */
+//register
 router.post("/register", (req, res, next) => {
   User.create(req.body, (err, newUser) => {
     if (err) return res.json(err);
@@ -17,9 +17,15 @@ router.post("/login", (req, res, next) => {
   var email = req.body.email;
   var password = req.body.password;
 
-  User.findOne({email},(err,user)=>{
-    if()
-  })
+  User.findOne({ email }, (err, user) => {
+    if (err) return res.json({ msg: "User not found" });
+    if (!user) return res.json({ msg: "User not registered" });
+    if (!user.validatePassword(password))
+      returnres.json({ msg: "Incorrect password" });
+    var token = jwt.sign({ userId: user._id }, process.env.secret);
+    return res.json({ token });
+  });
 });
+router.use(authToken.authToken);
 
 module.exports = router;
