39009312dd33ff333dadd7bb622ca80164148a4a
Abhinav <singh.abhinav6996@gmail.com>
2019-09-13 17:51:34 +0530

    routes update

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,25 @@
 var express = require("express");
 var User = require("../../models/User");
 var jwt = require("jsonwebtoken");
-var authToken = require;
+var authToken = require("../../modules/authToken");
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function(req, res, next) {
-  res.send("respond with a resource");
+router.post("/register", (req, res, next) => {
+  User.create(req.body, (err, newUser) => {
+    if (err) return res.json(err);
+    return res.json({ newUser });
+  });
+});
+
+//login
+router.post("/login", (req, res, next) => {
+  var email = req.body.email;
+  var password = req.body.password;
+
+  User.findOne({email},(err,user)=>{
+    if()
+  })
 });
 
 module.exports = router;
