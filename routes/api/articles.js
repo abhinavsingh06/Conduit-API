8e2321efd0a2f755feefd18eb27ecb01aac4bb0b
Abhinav <singh.abhinav6996@gmail.com>
2019-09-13 19:47:04 +0530

    articles CRUD update

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -22,3 +22,26 @@ router.get("/:id", (req, res, next) => {
     return res.json({ article });
   });
 });
+
+//update an article
+router.put("/update/:id", (req, res, next) => {
+  var id = req.params.id;
+  var loggedInUser = req.userid;
+
+  Article.findById(id, (err, article) => {
+    if (err) return res.json({ msg: "Could not find the article" });
+    if (loggedInUser == article.userid) {
+      Article.findByIdAndUpdate(
+        id,
+        req.body,
+        { new: true },
+        (err, updatedArticle) => {
+          if (err) return res.json({ msg: "could not update article" });
+          return res.json({ updatedArticle });
+        }
+      );
+    } else {
+      return res.json({ msg: "You are not authorized to edit this article" });
+    }
+  });
+});
