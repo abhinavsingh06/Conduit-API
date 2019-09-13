cf87aad89c3bf80c503c1b87b2c4510abecfc1ef
Abhinav <singh.abhinav6996@gmail.com>
2019-09-13 11:54:50 +0530

    model update

--- models/User.js
+++ models/User.js
@@ -1,2 +1,19 @@
 var bcrypt = require("bcrypt");
 var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  username: {},
+  email: {},
+  password: {},
+  profilePicture: String,
+  bio: String,
+  followers: {},
+  following: {},
+  articlesId: {},
+  commentsId: {}
+});
+
+var User = mongoose.model("User", userSchema);
+module.exports = User;
