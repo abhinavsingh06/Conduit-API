23c5d8981f5420698194d03a0667fe7dce45ff1e
Abhinav <singh.abhinav6996@gmail.com>
2019-09-15 19:52:28 +0530

    comments CRUD update

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -3,3 +3,12 @@ var Comment = require("../../models/Comment");
 var Article = require("../../models/Article");
 var authToken = require("../../modules/authToken");
 var router = express.Router();
+
+//read comments using article id
+router.get("/:articleId", (req, res, next) => {
+  var id = req.params.articleId;
+  Comment.find({ articleId: id }, (err, comments) => {
+    if (err) return res.json({ msg: "could not find comments" });
+    res.json({ comments });
+  });
+});
